f9eed76186f74f15fc33308d90f416f0b98be1d5
Kill-4 <agustinherrera043@gmail.com>
2024-06-30 00:31:40 -0300

    cambios para el render

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+//Levantar nuestro servidor
+constport = process.env.PORT || 3001;
+app.listen(port, ()=>console.log('servidor corriendo en el puerto ${port}'));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
